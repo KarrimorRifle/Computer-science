377f8e2b12d675588af0beced6a0900842b21167
KarrimorRifle <samuelto888@yahoo.co.uk>
2022-02-17 15:14:02 +0000

    bullets
    i can shoot now

--- Computer Science - Coursework/Assets/Scripts/GunController.cs
+++ Computer Science - Coursework/Assets/Scripts/GunController.cs
@@ -18,11 +18,14 @@ public class GunController : MonoBehaviour
     public bool reloading = false;
     public bool turret = false;
     public float reloadLength = 2.5f;
-    float reloadTime;
     Vector2 mousePos;
+    //shooting
+    public float bps = 3f; //bullets per second
+    public float lastFired = 0;
     void Start()
     {
         magCount = magMax;
+        Physics2D.IgnoreLayerCollision(3,8,true);
     }
     void FixedUpdate()
     {
@@ -42,22 +45,54 @@ public class GunController : MonoBehaviour
         else
             turret = false;
         anim.SetBool("showing",turret);
+        if(turret && Input.GetMouseButton(0))
+        {
+
+            shoot();
+        }
+        if(turret && Input.GetButtonDown("r")){
+            StartCoroutine(reload());
+            Debug.Log("Combat : reloading!");
+        }
+    }
+    public GameObject bulletPrefab;
+    void shoot()
+    {
+        if((magCount > 0) && (1/bps < (Time.time - lastFired)))
+        {
+            magCount --;
+            lastFired = Time.time;
+            GameObject bullet = Instantiate(bulletPrefab,shootLocation.position,shootLocation.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>(); //referencing the rigidbody
+            Transform bTrans = bullet.GetComponent<Transform>();
+            bTrans.Rotate(0,0,-90);
+            bullet.GetComponent<Bullet>().bKnockBack = gunKnockback;
+            bullet.GetComponent<Bullet>().bDamage = gunDamage;
+            rb.AddForce(((mousePos - (Vector2)shootLocation.position).normalized) * bulletVelocity, ForceMode2D.Impulse); //adding force to the rigidbody
+        }else if(magCount == 0)
+            Debug.Log("Combat: No ammo");
     }
     public IEnumerator reload()
     {
+        //Debug.Log("Combat : reloading 0");
         if(magCount < magMax)
         {
+            //Debug.Log("Combat : reloading 1");
             if(ammoCount >= magMax) //checks if there is enough ammo
             {
+                //Debug.Log("Combat : reloading 2");
                 reloading = true;
                 yield return new WaitForSeconds(reloadLength);
                 magCount = magMax;
                 ammoCount -= magMax;
+                reloading = false;
             }else if(ammoCount > 0){//in case there isnt enough ammo
+                //Debug.Log("Combat : reloading 3");
                 reloading = true;
                 yield return new WaitForSeconds(reloadLength);
                 magCount = ammoCount;
                 ammoCount = 0;
+                reloading = false;
             }
         }
    }
