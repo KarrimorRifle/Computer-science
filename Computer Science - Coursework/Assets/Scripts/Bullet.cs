ca0693423af60aa9cffac2e5264bc344fbfc7740
KarrimorRifle <samuelto888@yahoo.co.uk>
2022-02-18 19:18:03 +0000

    bullet edit

--- Computer Science - Coursework/Assets/Scripts/Bullet.cs
+++ Computer Science - Coursework/Assets/Scripts/Bullet.cs
@@ -7,12 +7,16 @@ public class Bullet : MonoBehaviour
     public float bDamage;
     public float bKnockBack;
     public GameObject hitExplosion;
+    void Start()
+    {
+        Destroy(gameObject,3f);
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.layer == 7)
             collision.gameObject.GetComponent<CombatClass>().takeDamage(bDamage,transform.position,bKnockBack);//calling the enemy's function
         GameObject explosion = Instantiate(hitExplosion, transform.position - new Vector3(0f,0.3f,0f), Quaternion.identity);
-        Destroy(explosion,1f);//destroy explosion after 2.5 seconds
+        Destroy(explosion,1/6);//destroy explosion after 2.5 seconds
         Destroy(gameObject);//destroy itself
     }
 }
